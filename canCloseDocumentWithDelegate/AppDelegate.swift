cb1d1ebe578e067723697d5116772c281a1c9aaf
Douglas Heriot <douglas@douglasheriot.com>
2015-12-02 09:40:56 +1100

    Initial Commit

---
+++ canCloseDocumentWithDelegate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  canCloseDocumentWithDelegate
+//
+//  Created by Douglas Heriot on 2/12/2015.
+//  Copyright © 2015 Douglas Heriot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
